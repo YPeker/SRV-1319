623b64d0228e716df73a1db690cf0c2be41f9bda
ypeker <ypeker@Ubuntu.myguest.virtualbox.org>
2022-10-31 13:43:59 +0100

    feat: decrpyt secret and write to file

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,17 @@
-console.log("Here we go!");
+import { createDecipheriv  } from "crypto"
+import { createWriteStream, readFileSync } from "fs"
+import { PassThrough } from "stream"
+import { createUnzip } from "zlib"
+
+const iv = readFileSync('iv.txt')
+const secret = readFileSync('secret.enc')
+const secretKey = readFileSync('secret.key', 'utf-8').substring(0, 32)
+const decrypt = createDecipheriv('aes-256-gcm', secretKey, iv)
+
+const decryptedData = decrypt.update(secret)
+
+const fileStreamOut = createWriteStream('clear.txt')
+var bufferStream = new PassThrough();
+bufferStream.end(decryptedData)
+bufferStream.pipe(createUnzip()).pipe(fileStreamOut)
+console.log('Done.')
