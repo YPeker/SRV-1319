863b14e62888ddbccda36d710b64731ae435b46e
ypeker <ypeker@Ubuntu.myguest.virtualbox.org>
2022-10-31 14:21:44 +0100

    feat: initial solution for task 2 and 3

--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,5 @@
-import { createDecipheriv  } from "crypto"
-import { createWriteStream, readFileSync } from "fs"
+import { createDecipheriv } from "crypto"
+import { createReadStream, createWriteStream, readFileSync } from "fs"
 import { PassThrough } from "stream"
 import { createUnzip } from "zlib"
 
@@ -10,8 +10,41 @@ const decrypt = createDecipheriv('aes-256-gcm', secretKey, iv)
 
 const decryptedData = decrypt.update(secret)
 
-const fileStreamOut = createWriteStream('clear.txt')
 var bufferStream = new PassThrough();
 bufferStream.end(decryptedData)
-bufferStream.pipe(createUnzip()).pipe(fileStreamOut)
-console.log('Done.')
+bufferStream.pipe(createUnzip())
+
+let sumOfAllNumbers = 0
+bufferStream.on('data', function (chunk: string) {
+    const stringChunk = chunk.toString()
+    for (const char of stringChunk) {
+        const possibleNumber = Number.parseInt(char)
+        if (!Number.isNaN(possibleNumber)) {
+            sumOfAllNumbers += possibleNumber
+            continue
+        }
+
+        switch (char) {
+            case 'a':
+                sumOfAllNumbers += 2
+                break;
+            case 'e':
+                sumOfAllNumbers += 4
+                break;
+            case 'i':
+                sumOfAllNumbers += 8
+                break;
+            case 'o':
+                sumOfAllNumbers += 16
+                break;
+            case 'u':
+                sumOfAllNumbers += 32
+                break;
+            default:
+                break;
+        }
+    }
+}).on('end', function () {
+    console.log('Sum of all numbers is:', sumOfAllNumbers);
+    console.log('Done');
+});
