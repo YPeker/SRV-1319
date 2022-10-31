fa99d5ec6222ff9bf4e94587aeaf349bf55f5004
ypeker <ypeker@Ubuntu.myguest.virtualbox.org>
2022-10-31 16:22:11 +0100

    feat: initial idea for task 4

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,8 @@
 import { createDecipheriv } from "crypto"
-import { createReadStream, createWriteStream, readFileSync } from "fs"
+import { readFileSync } from "fs"
 import { PassThrough } from "stream"
 import { createUnzip } from "zlib"
+import { createServer } from 'https'
 
 const iv = readFileSync('iv.txt')
 const secret = readFileSync('secret.enc')
@@ -10,41 +11,69 @@ const decrypt = createDecipheriv('aes-256-gcm', secretKey, iv)
 
 const decryptedData = decrypt.update(secret)
 
-var bufferStream = new PassThrough();
+const numberArr: number[] = []
+const totalAmountArr: number[] = []
+var bufferStream = new PassThrough()
 bufferStream.end(decryptedData)
-bufferStream.pipe(createUnzip())
-
-let sumOfAllNumbers = 0
-bufferStream.on('data', function (chunk: string) {
+bufferStream.pipe(createUnzip()).on('data', function (chunk) {
     const stringChunk = chunk.toString()
-    for (const char of stringChunk) {
-        const possibleNumber = Number.parseInt(char)
-        if (!Number.isNaN(possibleNumber)) {
-            sumOfAllNumbers += possibleNumber
-            continue
+    const sentences = stringChunk.split(/[\.\?\!]/)
+    for (const sentence of sentences) {
+        let sumOfAllNumbersInSentence = 0
+        let sumOfAllLetterValuesInSentence = 0
+        for (const char of sentence) {
+            const possibleNumber = Number.parseInt(char)
+            if (!Number.isNaN(possibleNumber)) {
+                sumOfAllNumbersInSentence += possibleNumber
+                continue
+            }
+        
+            switch (char) {
+                case 'a':
+                    sumOfAllLetterValuesInSentence += 2
+                    break
+                case 'e':
+                    sumOfAllLetterValuesInSentence += 4
+                    break
+                case 'i':
+                    sumOfAllLetterValuesInSentence += 8
+                    break
+                case 'o':
+                    sumOfAllLetterValuesInSentence += 16
+                    break
+                case 'u':
+                    sumOfAllLetterValuesInSentence += 32
+                    break
+                default:
+                    break
+            }
         }
-
-        switch (char) {
-            case 'a':
-                sumOfAllNumbers += 2
-                break;
-            case 'e':
-                sumOfAllNumbers += 4
-                break;
-            case 'i':
-                sumOfAllNumbers += 8
-                break;
-            case 'o':
-                sumOfAllNumbers += 16
-                break;
-            case 'u':
-                sumOfAllNumbers += 32
-                break;
-            default:
-                break;
-        }
+        totalAmountArr.push(sumOfAllNumbersInSentence+sumOfAllLetterValuesInSentence)
+        numberArr.push(sumOfAllNumbersInSentence)
     }
 }).on('end', function () {
-    console.log('Sum of all numbers is:', sumOfAllNumbers);
-    console.log('Done');
-});
+    const sumOfAllNumbers = totalAmountArr.reduce((previous,current) => previous+current,0)
+    let numberArrWithIndex = numberArr.map((value,index) => {return {value, index}})
+    numberArrWithIndex.sort((a,b) => b.value-a.value)
+
+    numberArrWithIndex = numberArrWithIndex.slice(0,10)
+    numberArrWithIndex.sort((a,b) => a.index-b.index)
+    const numbersSubstractedWithIndex= numberArrWithIndex.map(( item, index) =>  item.value- index)
+
+    console.log('Sum of all numbers and letter values is:', sumOfAllNumbers)
+    const solution =  String.fromCharCode(...numbersSubstractedWithIndex)
+
+    console.log({solution})
+    console.log('Starting web server')
+    const options = {
+        key: readFileSync('localhost.key'),
+        cert: readFileSync('localhost.crt')
+      }
+      
+      createServer(options, function (req, res) {
+        res.writeHead(200)
+        res.end(solution)
+      }).listen(8000)
+})
+
+
