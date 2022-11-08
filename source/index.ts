bb732c0dc4a6af8fe997cd72bd0720fea1f225d9
ypeker <ypeker@Ubuntu.myguest.virtualbox.org>
2022-11-08 11:51:36 +0100

    test

--- source/index.ts
+++ source/index.ts
@@ -1,83 +1,107 @@
-import { createDecipheriv } from "crypto"
-import { createReadStream, readFileSync } from "fs"
-import { PassThrough } from "stream"
-import { createUnzip } from "zlib"
-import { createServer } from 'https'
+import { parse } from 'csv-parse'
+import { createReadStream, appendFileSync, writeFile } from 'fs';
 
-const iv = readFileSync('iv.txt')
-const secret = readFileSync('secret.enc')
-const secretKey = readFileSync('secret.key', 'utf-8').substring(0, 32)
-const decrypt = createDecipheriv('aes-256-gcm', secretKey, iv)
 
-const decryptedData = decrypt.update(secret)
-
-const numberArr: number[] = []
-const totalAmountArr: number[] = []
-var bufferStream = new PassThrough()
-bufferStream.end(decryptedData)
-bufferStream.pipe(createUnzip())
-
-createReadStream('clear_smaller.txt').on('data', function (chunk) {
-    const stringChunk = chunk.toString()
-    const sentences = stringChunk.split(/[\.\?\!]/)
-    for (let i = 0; i < sentences.length; i++) {
-        const sentence = sentences[i];
-        let sumOfAllNumbersInSentence = 0
-        let sumOfAllLetterValuesInSentence = 0
-        for (let t = 0; t < sentence.length; t++) {
-            const char = sentence[t];            
-            const possibleNumber = Number.parseInt(char)
-            if (!Number.isNaN(possibleNumber)) {
-                sumOfAllNumbersInSentence += possibleNumber
-                continue
-            }
-        
-            switch (char) {
-                case 'a':
-                    sumOfAllLetterValuesInSentence += 2
-                    break
-                case 'e':
-                    sumOfAllLetterValuesInSentence += 4
-                    break
-                case 'i':
-                    sumOfAllLetterValuesInSentence += 8
-                    break
-                case 'o':
-                    sumOfAllLetterValuesInSentence += 16
-                    break
-                case 'u':
-                    sumOfAllLetterValuesInSentence += 32
-                    break
-                default:
-                    break
-            }
-        }
-        totalAmountArr.push(sumOfAllNumbersInSentence+sumOfAllLetterValuesInSentence)
-        numberArr.push(sumOfAllNumbersInSentence)
-    }
-}).on('end', function () {
-    const sumOfAllNumbers = totalAmountArr.reduce((previous,current) => previous+current,0)
-    let numberArrWithIndex = numberArr.map((value,index) => {return {value, index}})
-    numberArrWithIndex.sort((a,b) => b.value-a.value)
-
-    numberArrWithIndex = numberArrWithIndex.slice(0,10)
-    numberArrWithIndex.sort((a,b) => a.index-b.index)
-    const numbersSubstractedWithIndex= numberArrWithIndex.map(( item, index) =>  item.value- index)
-
-    console.log('Sum of all numbers and letter values is:', sumOfAllNumbers)
-    const solution =  String.fromCharCode(...numbersSubstractedWithIndex)
-
-    console.log({solution})
-    console.log('Starting web server')
-    const options = {
-        key: readFileSync('localhost.key'),
-        cert: readFileSync('localhost.crt')
-      }
-      
-      createServer(options, function (req, res) {
-        res.writeHead(200)
-        res.end(solution)
-      }).listen(8000)
+const parser = parse({
+    delimiter: ';',
+    fromLine: 2,
+    ignore_last_delimiters: true,
+    skip_empty_lines: true,
+    columns: ['tenantId', 'eMail', 'licenseStartDate', 'amountOfSMS', 'contractCode', "tenantData"],
 })
 
+type TenantBillData = {
+    tenantId: string,
+    eMail: string,
+    licenseStartDate: Date,
+    amountOfSMS: number,
+    contractCode: string,
+    tenantData: string,
+    parsedTenantData: {
+        userName: string,
+        userStartDate: Date,
+        userEndDate: Date,
+        userPartialBillAmount: number
+    }[]
+}
+
+type ResultBil = {
+    contractCode?: string
+    tenantIds: string[]
+    totalBill: number
+}
+
+const appendData= (bill: ResultBil) => {
+    appendFileSync('bill.csv', `${bill.contractCode};"${bill.tenantIds.map(x=>`${x}`)}";${bill.totalBill};\n`)
+}
+
+const records: Map<string,{tenantIds: string[], totalBill:number}> = new Map()
+
+writeFile('bill.csv', 'contractCode;tenantIds;totalBill\n',  (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+    }
+  } )
+parser.on('readable', function () {
+    let record: TenantBillData
+    while ((record = parser.read()) !== null) {
+        record.parsedTenantData = []
+        const valueArray = record.tenantData.split(';')
+        for (let index = 0; index < valueArray.length; index += 4) {
+            const userName: string = valueArray[index]
+            const userStartDate: Date = new Date(valueArray[index + 1])
+            const userEndDate: Date = new Date(valueArray[index + 2])
+            const userPartialBillAmount: number = Number.parseFloat(valueArray[index + 3])
+
+            record.parsedTenantData.push({
+                userName,
+                userStartDate,
+                userEndDate,
+                userPartialBillAmount,
+            })
+        }
+        // if there is no contract code skip the user
+        let totalBill =  record.parsedTenantData.map(x => x.userPartialBillAmount).reduce((x,y) => x +y, 0)
+        let allTenantIds = [record.tenantId]
+        if(record.contractCode ==='' || record.contractCode === null || record.contractCode === undefined) {
+            appendData({
+            tenantIds: [record.tenantId],
+            totalBill: totalBill,
+            })
+            continue
+        }
+        // if we already have the same contract code saved, add the previous value
+        if(records.has(record.contractCode)) {
+            totalBill += (records.get(record.contractCode)!.totalBill)
+            allTenantIds.push(...records.get(record.contractCode)!.tenantIds)
+        }
+        records.set(record.contractCode, {  
+            tenantIds:allTenantIds,
+            totalBill: totalBill
+        })
+    }
+
+});
+
+
+// Catch any error
+parser.on('error', function (err) {
+    console.error({ error: err.message }, 'ERROR');
+});
+
+parser.on('end', function () {
+    for (const iterator of records.entries()) {
+        const contractCode = iterator[0]
+        const tenantIdsAndTotalBill: {tenantIds: string[], totalBill:number} = iterator[1]
+        appendData({
+            contractCode,
+            tenantIds: tenantIdsAndTotalBill.tenantIds,
+            totalBill: tenantIdsAndTotalBill.totalBill
+        })
+    } 
+});
+
+createReadStream('./testTenants.csv').pipe(parser)
 
